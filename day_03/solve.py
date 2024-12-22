7d245648d600e24fbf99df8f246026a5abbfb450
Kevin Gillespie <kevin.gillespie@maximintegrated.com>
2024-12-22 10:03:07 -0600

    Finished with part 2.

--- day_03/solve.py
+++ day_03/solve.py
@@ -23,31 +23,76 @@ if args.verbose:
 else:   
     kprint = lambda *a: None      # do-nothing function
 
+
+def getSum(inputString):
+    sumPairs = 0
+
+    kprint(inputString)
+
+    # Split on "mul(", skip the first element
+    for item in inputString.split("mul(")[1:]:
+        # kprint(item)
+
+        # \d{1,3},\d{1,3}\)
+        match = re.match(r'\d{1,3},\d{1,3}\)', item)
+        if(match):
+
+            # Remove the ")"
+            pair = match.group(0)[:-1]
+            kprint(pair)
+
+            leftOp = int(pair.split(",")[0])
+            rightOp = int(pair.split(",")[1])
+
+            sumPairs = sumPairs + leftOp*rightOp
+    return(sumPairs)
+
 # Open the input file
 textFile = open(args.input, "r")
 inputString = textFile.read()
 
 kprint("inputString: ",inputString)
+kprint()
 
 sumPairs = 0
 
-# Split on "mul(", skip the first element
-for item in inputString.split("mul(")[1:]:
-    # kprint(item)
+if(args.part2):
+    
+    # Find the first "don't", mul is enabled to begin with
+    endIndex = inputString.index("don't()")
 
-    # \d{1,3},\d{1,3}\)
-    match = re.match(r'\d{1,3},\d{1,3}\)', item)
-    if(match):
+    # Get the sum for the first section
+    sumPairs = sumPairs + getSum(inputString[:endIndex])
 
-        # Remove the ")"
-        pair = match.group(0)[:-1]
-        kprint(pair)
+    # Seek to the end index
+    inputString = inputString[endIndex:]
 
-        leftOp = int(pair.split(",")[0])
-        rightOp = int(pair.split(",")[1])
 
-        sumPairs = sumPairs + leftOp*rightOp
+    while(inputString != None):
+
+        try:
+            # Seek to the start index
+            startIndex = inputString.index("do()")
+        except ValueError:
+            # "do" not found
+            break
+
+        inputString = inputString[startIndex:]
+
+        try:
+            endIndex = inputString.index("don't()")
+        except ValueError:
+            # "don't" isn't found
+            endIndex = len(inputString)
+
+
+        # Add the sumPairs
+        sumPairs = sumPairs + getSum(inputString[:endIndex])
+
+        # Seek to the end index
+        inputString = inputString[endIndex:]
+
+else:
+    sumPairs = getSum(inputString)
 
 print(sumPairs)
-
-
